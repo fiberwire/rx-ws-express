fb2bdac14e37ad262b6dc399221c5ef6c6694f77
fiberwire <andrew.ramos92@gmail.com>
2017-06-06 09:49:53 -0400

    exported more stuff from index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import { RequestResponse } from './request-response';
+import { Message } from './message';
+import { SocketExpressServer } from './socket-express-server';
 import { SocketServer } from './socket-server';
 import { Server } from './server';
+
 export {
     Server,
-    SocketServer
+    SocketServer,
+    SocketExpressServer,
+    Message,
+    RequestResponse
 }
